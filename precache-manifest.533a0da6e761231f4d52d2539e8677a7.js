cd165bef6f524c54f77e47296875db6794f83dfe
PDahiya123 <pranshudahiyaiitg@gmail.com>
2022-03-15 19:58:11 +0530

    Updates

---
+++ precache-manifest.533a0da6e761231f4d52d2539e8677a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf04991385a23a6667503f18f994516",
+    "url": "/Object-Detection-Web-App/index.html"
+  },
+  {
+    "revision": "8b9f769e365fa0bd6582",
+    "url": "/Object-Detection-Web-App/static/js/2.68118d9f.chunk.js"
+  },
+  {
+    "revision": "b17142fa0d95f40ed3dd5d6f162ed77b",
+    "url": "/Object-Detection-Web-App/static/js/2.68118d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbf73d3f83f5b2c55c53",
+    "url": "/Object-Detection-Web-App/static/js/main.81181675.chunk.js"
+  },
+  {
+    "revision": "9f063a8ddd72e1836fdf",
+    "url": "/Object-Detection-Web-App/static/js/runtime-main.dbe3c23f.js"
+  }
+]);
